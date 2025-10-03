7291fd2bc1c8da1cfaa16938d30c0e2d26bbde98
eredouan550-hub <eredouan550@gmail.com>
2025-10-03 04:57:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'calisthenics-app-v1';
+const urlsToCache = [
+  '/',
+  '/calisthenics-index.html',
+  '/calisthenics-beginner.html',
+  '/calisthenics-intermediate.html', 
+  '/calisthenics-advanced.html',
+  '/manifest.json',
+  '/sw.js'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
